dfa8b53d860fc16fa2c6e5cd2bea2ffe950d86d2
강도훈 <fghfgh3255@gmail.com>
2022-06-04 10:01:53 +0900

    Initial commit

---
+++ app/src/main/java/com/dohn/practice_220604/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dohn.practice_220604
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
